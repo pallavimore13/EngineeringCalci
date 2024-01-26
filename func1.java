678430b8e65700743956448f663225267caf5089
pallavimore13 <2022bit151@sggs.ac.in>
2024-01-27 00:32:32 +0530

    ADD

---
+++ func1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class calci{
+ public static double Add( double num1,double num2){
+   double sum=num1+num2;
+   return sum;
+   }
+  public static double Sub(double num1,double num2){
+   double result=num1-num2;
+   return result;
+  }
