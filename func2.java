2305496deeaf267c9a8a4048676ff5e88899ab11
pallavimore13 <2022bit151@sggs.ac.in>
2024-01-28 22:22:29 +0530

    add

---
+++ func2.java
@@ -0,0 +1,13 @@
+ public static double Mul(double num1,double num2){
+   double result=num1*num2;
+   return result;
+   }
+  public static double Div(double num1,double num2){
+  if(num2 != 0){
+  double result = num1/num2;
+  return result;
+  }
+  else{
+  return -1;
+  }
+  }
