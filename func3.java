24a38d6a75d8b9e06e63e40264c8862416fb1e45
pallavimore13 <2022bit151@sggs.ac.in>
2024-01-28 22:35:04 +0530

    Add

---
+++ func3.java
@@ -0,0 +1,18 @@
+ public static double modulo(double num1,double num2){
+   if(num2 != 0){
+   double result = num1%num2;
+   return result;
+   }
+   else{
+    return -1;
+    }
+    }
+   public static double squareroot(double num1){
+   if(num1>=0){
+    double result=Math.sqrt(num1);
+    return result;
+   }
+   else{
+     return -1;
+     }
+   }
